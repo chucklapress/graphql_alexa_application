a3f0d51995ffd30f67653eee3f87e129d0332dd6
chucklapress <charleslapress@gmail.com>
2019-12-16 17:15:58 -0500

    change to file

--- alexa_lambda/gql_lambda/index.js
+++ alexa_lambda/gql_lambda/index.js
@@ -1,5 +1,5 @@
 const { GraphQLClient } = require('graphql-request');
-const GRAPHQL_ENDPOINT = 'https://c99539d9.ngrok.io';
+const GRAPHQL_ENDPOINT = 'https://076f76c4.ngrok.io';
 const graphQLClient = new GraphQLClient(GRAPHQL_ENDPOINT);
 const helloWorldQuery = `{ hello }`;
 const Alexa = require('ask-sdk-core');
@@ -27,7 +27,7 @@ const HelloWorldIntentHandler = {
  async handle(handlerInput) {
  const response = await graphQLClient.request(helloWorldQuery);
 
- const speechText = `Your exciting announcement ${response.hello}`;
+ const speechText = `You were hoping to hear ${response.hello}`;
 
  return handlerInput.responseBuilder
  .speak(speechText)
