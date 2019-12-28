ca443ac4000aad88224b5da606eba12dd1fae071
chucklapress <charleslapress@gmail.com>
2019-12-28 17:44:18 -0500

    tested service using new ngrok address

--- alexa_lambda/gql_lambda/index.js
+++ alexa_lambda/gql_lambda/index.js
@@ -1,5 +1,5 @@
 const { GraphQLClient } = require('graphql-request');
-const GRAPHQL_ENDPOINT = 'https://f607fc96.ngrok.io';
+const GRAPHQL_ENDPOINT = 'https://405cd5e5.ngrok.io';
 const graphQLClient = new GraphQLClient(GRAPHQL_ENDPOINT);
 const helloWorldQuery = `{ hello }`;
 const Alexa = require('ask-sdk-core');
